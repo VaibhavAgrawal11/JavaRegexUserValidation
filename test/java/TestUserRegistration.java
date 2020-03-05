eb71014ff6ece147d270b030035256a1f88054a1
VaibhavAgrawal11 <vaibhavagrawal1110@gmail.com>
2020-03-05 14:36:06 +0530

    MODIFIED:Added feature to validate test cases for last name.

--- test/java/TestUserRegistration.java
+++ test/java/TestUserRegistration.java
@@ -3,6 +3,7 @@ import org.junit.Test;
 
 public class TestUserRegistration {
     UserValidation validator = new UserValidation();
+    //TEST CASES FOR VALIDATING FIRST NAME
     @Test
     public void givenFirstName_WhenProper_ShouldReturnTrue(){
         boolean result = validator.checkFirstName("Vailbhav");
@@ -14,4 +15,15 @@ public class TestUserRegistration {
         Assert.assertFalse(result);
     }
 
+    //TEST CASES FOR VALIDATING LAST NAME
+    @Test
+    public void givenLastName_WhenProper_ShouldReturnTrue(){
+        boolean result = validator.checklastName("Agrawal");
+        Assert.assertTrue(result);
+    }
+    @Test
+    public void givenLastName_WhenImproper_ShouldReturnFalse(){
+        boolean result = validator.checklastName("aGraHGwal");
+        Assert.assertFalse(result);
+    }
 }
