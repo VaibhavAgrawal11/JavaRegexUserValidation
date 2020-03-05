41bc3aab96fcf5dc8ebf25c4cb8c5aca4a12648f
VaibhavAgrawal11 <vaibhavagrawal1110@gmail.com>
2020-03-05 13:12:15 +0530

    ADDED: File with welcome message

---
+++ main/java/UserValidation.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UserValidation {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to java regex");
+    }
+}
