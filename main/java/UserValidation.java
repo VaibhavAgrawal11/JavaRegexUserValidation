6ed3158cb94d098925bc52c615bc1c50b96a1e17
VaibhavAgrawal11 <vaibhavagrawal1110@gmail.com>
2020-03-05 14:35:09 +0530

    MODIFIED: Added feature to validate last name.

--- main/java/UserValidation.java
+++ main/java/UserValidation.java
@@ -4,7 +4,8 @@ import java.util.regex.Pattern;
 
 public class UserValidation {
     public static void main(String[] args) {
-        System.out.println("Added feature to validate first name");
+        System.out.println("Added feature to validate last name");
+        //VALIDATING FIRST NAME
         System.out.println("Enter your first name:");
         Scanner sc = new Scanner(System.in);
         String firstName = sc.nextLine();
@@ -13,7 +14,24 @@ public class UserValidation {
             System.out.println("Valid first name");
         else
             System.out.println("Invalid first name");
+        //VALIDATING LAST NAME
+        System.out.println("Enter your last name:");
+        String lastName = sc.nextLine();
+        boolean lastNameCheck= checklastName(lastName);
+        if (lastNameCheck)
+            System.out.println("Valid last name");
+        else
+            System.out.println("Invalid last name");
+
     }
+
+    //VALIDATE THE LAST NAME
+    static boolean checklastName(String lastName) {
+         Pattern namePat = Pattern.compile("^[A-Z][a-z]{2,}");
+         Matcher check = namePat.matcher(lastName);
+         boolean lastNameCheck = check.matches();
+         return lastNameCheck;
+     }
 
     //VALIDATE THE FIRST NAME
     public static boolean checkFirstName(String firstName) {
